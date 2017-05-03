47379edbed67ae77bc04c3f836a54ee1bfb6f731
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2017-05-03 22:28:45 +0200

    Add examples of usage

--- src/bin/twitter-limits.js
+++ src/bin/twitter-limits.js
@@ -3,7 +3,10 @@
 import Captweet from '../lib/index.js';
 import args from 'args';
 
-args.option('ini-file', 'The path of your ini file');
+args
+  .option('ini-file', 'The path of your ini file')
+  .example('twitter limits', 'Print the whole Rate limit status')
+  .example('twitter limits --ini-file ~/ini.yaml', 'Print the whole Rate limit status, using a specific ini file');
 const flags = args.parse(process.argv);
 
 const captweet = new Captweet(flags.iniFile);
