835489b73a3419629b1af480d2bb61d75134731a
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2017-05-04 14:22:56 +0200

    Fixed #3 - Add git flow event

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 gulp &&
   npm version $1 &&
+  git flow release finish $2 &&
   github_changelog_generator &&
   git add CHANGELOG.md &&
   git commit -m "Update history of changes in CHANGELOG.md" &&
